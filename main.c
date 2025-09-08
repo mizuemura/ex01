c7dc9cb1a9b17ea54f60f7265f0141b7f688c23e
Juhun Lee <rangho@yonsei.ac.kr>
2025-09-08 14:20:28 +0900

    feat: implement simple calculator

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    (void)printf("Usage: %s <num> <op> <num>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  // FIXME: allow floating point numbers!
+  int lhs = atoi(argv[1]);
+  char op = argv[2][0];
+  int rhs = atoi(argv[3]);
+
+  int result;
+  switch (op) {
+  case '+':
+    result = lhs + rhs;
+    break;
+  case '-':
+    result = lhs - rhs;
+    break;
+  case '*':
+    result = lhs * rhs;
+    break;
+  case '/':
+    // FIXME: handle division by zero here!
+    result = lhs / rhs;
+    break;
+    
+  default:
+    (void)printf("Unknown operator: %c\n", op);
+    return EXIT_FAILURE;
+  }
+
+  (void)printf("%d %c %d = %d\n", lhs, op, rhs, result);
+  return EXIT_SUCCESS;
+}
